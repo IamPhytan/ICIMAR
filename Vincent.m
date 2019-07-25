8b69c4d0bc1f5bf9beb325fa81453e13a5ca47c3
Damien LaRocque <phicoltan@gmail.com>
2019-07-25 16:58:18 -0400

    Tried to refactor Vincent's code.
    God why MATLAB and this piece of code sucks

--- Vincent.m
+++ Vincent.m
@@ -34,14 +34,14 @@ classdef Vincent < handle
 
     % Robot Generique
     methods (Access = private)
-        function xyPins = robotGenerique(thisVincent, architecture,initialStates,lengths,xyThetaDotMax,xyThetaRequest,Planner,obstacles)
+        function xyPins = robotGenerique(thisVincent, manipulator, architecture, initialStates, xyThetaDotMax,xyThetaRequest,Planner,obstacles)
             % robotGenerique  Calcule les valeurs d'angle et de x/y pour déplacer le robot
             %   Prend l'architecture, les angles, les longueurs de membres, les vitesses maximales, les cibles, le planificateur et les obstacles
             %
             % :param thisVincent: cette instance de Vincent
+            % :param manipulator: bras à bouger
             % :param architecture: architecture du robot (char array)
             % :param initialStates: angles relatifs initiaux du robot (array)
-            % :param lengths: longueurs initales des membres du robot
             % :param xyThetaDotMax: vitesses maximales de l'organe terminal
             % :param xyThetaRequest: cibles du robot (struct array)
             % :param Planner: fonctions de planification à implémenter dans le cadre du cours Éléments de Robotique GMC-3351 / GMC-7046
@@ -49,77 +49,76 @@ classdef Vincent < handle
             % :returns thetaOut: final angles
             % :returns xyPins: joint coordinates
             % :returns Done: flag to indicate that the point was reached
-            %
 
-            if isempty(thisVincent.getThetaOutMemory())
-                thisVincent.setThetaOutMemory(initialStates);
-            end
 
+            % CONSTANTES LIÉES À L'ARCHITECTURE
             n=length(architecture);
-
-            % Condition initiales
-            div = 50;
-
-            % SÉRIES DE DONNÉES
-
-            % Position
-            x = zeros(1, n);
-            y = zeros(1, n);
-            phi = zeros(1, n);
-
-            % Vitesses
-            xdot = zeros(1, n);
-            ydot = zeros(1, n);
             thetadot = double(architecture' == 'R');
 
+            % Condition initiale
+            div = 50;
+            phi = manipulator.getMemberValues("radabsangle");
 
-            % CONDITONS INITIALES
-            phi(1)=thisVincent.thetaOutMemory(1);
-            x(1,1)=lengths(1)*cos(phi(1));
-            y(1,1)=lengths(1)*sin(phi(1));
+            angles = manipulator.getMemberValues("radrelangle");
 
-            xdot(1,1)=0-y(n);
-            ydot(1,1)=x(n)-0;
+            while div > 1
 
-            % Itération pour toute l'architecture
-            if n > 1
-                for jj=2:n
-                    % Calculs de positions
-                    phi(jj)=phi(jj-1)+thisVincent.thetaOutMemory(jj);
-                    x(1,jj)=x(jj-1)+lengths(jj)*cos(phi(jj));
-                    y(1,jj)=y(jj-1)+lengths(jj)*sin(phi(jj));
+                manipulator.render();
 
-                    % Calculs de vitesses (JACOBIENNE)
-                    xdot(1,jj)=y(jj-1)-y(n);
-                    ydot(1,jj)=x(n)-x(jj-1);
+                % SÉRIES DE DONNÉES
+                % Position
+                x = manipulator.getMemberValues("endx");
+                y = manipulator.getMemberValues("endy");
+
+                % TODO: Unpseudocodize this line
+                current = [eFFector'; manipulator.members(end).radabsangle];
+
+                % Vitesses
+                xdot = zeros(1, n);
+                ydot = zeros(1, n);
+
+                xdot(1,1)=0-y(n);
+                ydot(1,1)=x(n)-0;
+
+                % Itération pour toute l'architecture
+                if n > 1
+                    for jj=2:n
+                        % Calculs de vitesses (JACOBIENNE)
+                        xdot(1,jj)=y(jj-1)-y(n);
+                        ydot(1,jj)=x(n)-x(jj-1);
+                    end
                 end
+
+                % Construction de la jacobienne
+                J=[xdot; ydot; thetadot];
+
+                % Limiteur de vitesses
+                xyThetaDot=xyThetaRequest-current;
+                div=max(abs(xyThetaDot)./xyThetaDotMax);
+                xyThetaDot = xyThetaDot / (div^(div > 1));
+
+                % Calculs prochain pas de temps et fonction de planification avec
+                % projection dans le noyeau
+                angles = Planner(angles, J, obstacles, xyThetaDot);
+
+
+                % TODO: Unpseudocodize this part
+                % FIXME: Broke it
+                lengths = manipulator.getLengths
+
+                xyPins = zeros(1, n);
+                xyPins = [arm.getX(); arm.getY()] * xyPins;
+                for num in 2:range(n):
+                    xyPins(:, num) = [xyPins(1, num-1) + length(num) * cos(angles(num)); xyPins(1, num-1) + length(num) * cos(angles(num))];
+
+
+                return xvPins into arm
+
+                % TODO: Set Values inside arm
+                % TODO: Set Angles == > xyPins
             end
 
-            % Construction de la jacobienne
-            J=[xdot, ydot, thetadot];
-
-            thetaOut=thisVincent.getThetaOutMemory();
-
-            xyPins=[x;y];
-
-
-            % TODO: Reimplement with a while loop
-
-            % WTF is going on here
-
-            % Limiteur de vitesses (Créer Lignes droites en meme temps)
-            xyThetaDot=xyThetaRequest-[xyPins(:,n);phi(n)];
-            div=max(abs(xyThetaDot)./xyThetaDotMax);
-            if(div > 1)
-                xyThetaDot=xyThetaDot/div;
-                Done=0;
-            else
-                Done=1;
-                %xyThetaDot=xyThetaRequest;
-            end
-            % Calculs prochain pas de temps et fonction de plnaification avec
-            % projection dans le noyeau
-            [thetaOutMemory]=Planner(thetaOutMemory,J,obstacles,xyThetaDot);
+            manipulator.render()
         end
     end
 
@@ -132,25 +131,40 @@ classdef Vincent < handle
             % reachTargets  Reach predefined targets
             %   Call robotGenerique with proper arguments
 
+            % Constantes
             architecture = (reachingArm.getMemberValues("architecture"))';
-            initialStates = (reachingArm.getMemberValues("relangle"))';
-            lengths = ;
             maximalEndSpeeds = reachingArm.getMaxEndSpeeds();
-
-
-
-
-            % Get request list
-            targetsToReach = reachingArm.getTargets();
-            nTargets = length(targetsToReach);
-            request = zeros(1, nTargets);
-            for iTarget=1:nTargets
-                target = targetsToReach(iTarget);
-                request(iTarget) = [target.x ; target.y; target.theta];
-            end
-
             plannerHandle = reachingArm.getPlannerFunc();
 
+            % Get target list
+            armTargets = reachingArm.getTargets();
+            nTargets = length(armTargets);
+            targetsList = zeros(3, nTargets);
+            for iTarget=1:nTargets
+                target = armTargets(iTarget);
+                targetsList(:, iTarget) = [target.x ; target.y; deg2rad(target.theta)];
+            end
+
+            % Get obstacles list
+            armObstacles = reachingArm.getObstacles();
+            nObstacles = length(armObstacles);
+            obstaclesList = zeros(3, nObstacles);
+            for iObstacle=1:nObstacles
+                obstacle = armObstacles(iObstacle);
+                obstaclesList(:, iObstacle) = [obstacle.x ; obstacle.y; obstacle.radius];
+            end
+
+
+            for iRequest=1:nTargets
+                % Variables
+                initialStates = (reachingArm.getMemberValues("relangle"))';
+
+                request = targetsList(:, iRequest);
+
+                xyCoords = robotGenerique();
+
+
+            end
 
 
 
@@ -160,13 +174,6 @@ classdef Vincent < handle
 
 
 
-
-
-            % :param lengths: longueurs initales des membres du robot
-            % :param xyThetaDotMax: vitesses maximales de l'organe terminal
-            % :param xyThetaRequest: cibles du robot (struct array)
-            % :param Planner: fonctions de planification à implémenter dans le cadre du cours Éléments de Robotique GMC-3351 / GMC-7046
-            % :param obstacles: obstacles sur le parcours du robot
             % :returns thetaOut: final relative angles
             % :returns xyPins: joint coordinates
             % :returns Done: flag to indicate that the point was reached
