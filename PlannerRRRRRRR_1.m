ba42c9ff54a3f4d3cdb8888d79211535684c46bc
Damien LaRocque <phicoltan@gmail.com>
2019-08-06 15:43:51 -0400

    Refactor with Vincent's code for P joints

--- PlannerRRRRRRR_1.m
+++ PlannerRRRRRRR_1.m
@@ -1,10 +1,13 @@
-function [out,Done] = PlannerRRRRRRR_1(CurrentJointAngles,J,Obstacles,xyThetaDot)
+function [out,Done] = PlannerRRRRRRR_1(CurrentJointValues,J,Obstacles,xyThetaDot)
 % PlannerRRRRRRR_1 Fonction de planification du manipulateur sériel
 %   L'implémentation de base du planificateur est la suivante :
 %       `out=CurrentJointAngles+pinv(J)*xyThetaDot;`
 
 % Fonction Objective
-out=CurrentJointAngles+pinv(J)*xyThetaDot;
+% out=CurrentJointAngles+pinv(J)*xyThetaDot;
+
+% Pseudo Inverse
+out=CurrentJointValues+(J'*inv(J*J'))*xyThetaDot;
 
 %out=CurrentJointAngles;
 end
--- Vincent.m
+++ Vincent.m
@@ -15,7 +15,7 @@ classdef Vincent < handle
 
     % Robot Generique
     methods (Access = private)
-        function robotGenerique(~, manipulator, targetIdx, architecture, xyThetaRequest, xyThetaDotMax, obstacles, Planner)
+        function robotGenerique(~, manipulator, targetIdx, architecture, xyThetaRequest, maxEndSpeeds, obstacles, Planner)
             % robotGenerique  Calcule les valeurs d'angle et de x/y pour déplacer le robot
             %   Prend l'architecture, les cibles, les vitesses maximales, les obstacles et le planificateur.
             %
@@ -23,45 +23,50 @@ classdef Vincent < handle
             % :param targetIdx: indice de la cible
             % :param architecture: architecture du robot (char array)
             % :param xyThetaRequest: cible du robot (struct array)
-            % :param xyThetaDotMax: vitesses maximales de l'organe terminal
+            % :param maxEndSpeeds: vitesses maximales de l'organe terminal
             % :param obstacles: obstacles sur le parcours du robot
             % :param Planner: fonctions de planification à implémenter dans le cadre du cours Éléments de Robotique GMC-3351 / GMC-7046
 
 
             % CONSTANTES LIÉES À L'ARCHITECTURE
             n=length(architecture);
-            thetadot = double(architecture' == 'R');
+            jointsR = (architecture == 'R');
+            jointsP = ~jointsR;
+            thetadot = double(jointsR');
 
             % Condition initiale
             div = 50;
             mvmt_idx = 0;
 
             % TODO: Chaotic
+            % Variables évolutives
             relAngles = (deg2rad(manipulator.getMemberValues("relangle")))';
+            totLengths = (manipulator.getMemberValues("totalLength"))';
 
             while div > 1
 
                 % SÉRIES DE DONNÉES
                 % Position
+                % FIXME: Possibilité de devoir changer le calcul de ces valeurs
                 x = manipulator.getMemberValues("endx");
                 y = manipulator.getMemberValues("endy");
-
                 absoluteAngles = deg2rad(manipulator.getMemberValues("absangle"));
                 currentPos = [(manipulator.getEndEffector())'; absoluteAngles(end)];
 
                 % Vitesses
+                % Calculs de vitesses (JACOBIENNE)
                 xdot = zeros(1, n);
                 ydot = zeros(1, n);
 
-                xdot(1,1)=0-y(n);
-                ydot(1,1)=x(n)-0;
-
-                % Itération pour toute l'architecture
-                if n > 1
-                    for jj=2:n
-                        % Calculs de vitesses (JACOBIENNE)
-                        xdot(1,jj)=y(jj-1)-y(n);
-                        ydot(1,jj)=x(n)-x(jj-1);
+                for jj=1:n
+                    currentR = jointsR(jj);
+                    currentP = ~currentR;
+                    if jj == 1
+                        xdot(1,jj) = currentR * (0 - y(n)) + currentP * cos(absoluteAngles(jj));
+                        ydot(1,jj) = currentR * (x(n) - 0) + currentP * sin(absoluteAngles(jj));
+                    else
+                        xdot(1,jj) = currentR * (y(jj-1) - y(n)) + currentP * cos(absoluteAngles(jj));
+                        ydot(1,jj) = currentR * (x(n) - x(jj-1)) + currentP * sin(absoluteAngles(jj));
                     end
                 end
 
@@ -70,32 +75,42 @@ classdef Vincent < handle
 
                 % Limiteur de vitesses
                 xyThetaDot=xyThetaRequest-currentPos;
-                div=max(abs(xyThetaDot)./xyThetaDotMax);
+
+                div=max(abs(xyThetaDot)./maxEndSpeeds);
                 xyThetaDot = xyThetaDot / (div^(div > 1));
-                size(xyThetaDot);
 
                 % Calculs du prochain état par fonction de planification avec projection dans le noyau
-                relAngles = Planner(relAngles, J, obstacles, xyThetaDot);
+                variableData = jointsR .* relAngles + jointsP .* totLengths;
+                resData = Planner(variableData, J, obstacles, xyThetaDot);
 
-                % Convert angles in XY Pins
-                memberLengths = manipulator.getMemberValues("totalLength");
-                newAbsAngles = cumsum(relAngles);
+                relAngles = jointsR .* resData + jointsP .* relAngles;
+                totLengths = jointsP .* resData + jointsR .* totLengths;
 
-                xyPins = ones(1, n + 1);
-                xyPins = [manipulator.getX(); manipulator.getY()] * xyPins;
-                for iMember=1:n
-                    xyPins(:, iMember + 1) = [memberLengths(iMember) * cos(newAbsAngles(iMember));
-                                        memberLengths(iMember) * sin(newAbsAngles(iMember))];
-                end
+                % % Set Params from Lengths and Angles
+                manipulator.setFromTotLenRelAng(totLengths, relAngles);
 
-                xValues = cumsum(xyPins(1, :));
-                yValues = cumsum(xyPins(2, :));
 
-                manipulator.setParamsFromXandY(xValues, yValues);
+
+                % % % Convert angles in XY Pins
+                % memberLengths = totLengths;
+                % newAbsAngles = cumsum(relAngles);
+
+                % xyPins = ones(1, n + 1);
+                % xyPins = [manipulator.getX(); manipulator.getY()] * xyPins;
+                % for iMember=1:n
+                %     xyPins(:, iMember + 1) = [memberLengths(iMember) * cos(newAbsAngles(iMember));
+                %                         memberLengths(iMember) * sin(newAbsAngles(iMember))];
+                % end
+
+                % xValues = cumsum(xyPins(1, :));
+                % yValues = cumsum(xyPins(2, :));
+
+                % manipulator.setParamsFromXandY(xValues, yValues);
 
                 % Show result
                 manipulator.render();
 
+                % Stop at 500 movements
                 mvmt_idx = mvmt_idx + 1;
 
                 if mvmt_idx == 500
