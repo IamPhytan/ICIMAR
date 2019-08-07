627d23dfe1dc0de6df8aab40c97f0978fea346ee
Damien LaRocque <phicoltan@gmail.com>
2019-08-07 12:09:57 -0400

    Rename first planner file, so it's explicit

--- Planner_RRRRRRR_1.m
+++ Planner_RRRRRRR_1.m
@@ -1,4 +1,4 @@
-function [out,Done] = PlannerRRRRRRR_1(CurrentJointValues,J,Obstacles,xyThetaDot)
+function out = Planner_RRRRRRR_1(CurrentJointValues,J,Obstacles,xyThetaDot)
 % PlannerRRRRRRR_1 Fonction de planification du manipulateur sériel
 %   L'implémentation de base du planificateur est la suivante :
 %       `out=CurrentJointAngles+pinv(J)*xyThetaDot;`
