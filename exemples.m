694ead38ca8a0f8cf8be4e2556b99a5e4464d41f
Damien LaRocque <phicoltan@gmail.com>
2019-08-08 14:33:55 -0400

    Ajouter notes à l'Exemple 1

--- exemples.m
+++ exemples.m
@@ -8,7 +8,8 @@ clc
 % ================================ EXEMPLES ================================
 
 %
-% Exemple 1
+% Exemple 1 - Atteindre un point avec un robot PPPR
+%   Il est à noter qu'un des joints prismatiques aura une longueur négative.
 %
 
 % Architecture : ['P', 'P', 'P', 'R']
@@ -29,8 +30,8 @@ clc
 % ]
 
 axisLimits = [
-    [-1, 3];
-    [ 0, 3]
+    [-1, 4];
+    [-1, 3]
 ];
 
 
@@ -48,6 +49,50 @@ ICIMAR('Planner_PosOnly', 'Exemple1', axisLimits)
 
 % ICIMAR Planner_Critere_mid_Prismatic Exemple1
 ICIMAR('Planner_Critere_mid_Prismatic', 'Exemple1', axisLimits)
+
+
+%
+% Exemple 2 - Atteindre un point avec un robot R x n
+%   Passage dans les obstacles
+%
+
+% Architecture : ['P', 'P', 'P', 'R']
+% Longueurs : [1, 1, 1, 1]
+% Angles : [0, 90, 90, 90]
+% Vitesse cartésiennes maximales de l'organe terminal : [0.03, 0.03, 3]
+% Cibles à atteindre : [
+%     {
+%         x : 1,
+%         y : 1,
+%         angle : 270
+%     },
+%     {
+%         x : 1.5,
+%         y : 1.5,
+%         angle : 180
+%     }
+% ]
+
+% axisLimits = [
+%     [-1, 3];
+%     [ 0, 3]
+% ];
+
+
+% %
+% % a) Planner_PosOnly
+% %
+
+% % ICIMAR Planner_PosOnly Exemple1
+% ICIMAR('Planner_PosOnly', 'Exemple1', axisLimits)
+
+
+% %
+% % b) Planner_Critere_mid_Prismatic
+% %
+
+% % ICIMAR Planner_Critere_mid_Prismatic Exemple1
+% ICIMAR('Planner_Critere_mid_Prismatic', 'Exemple1', axisLimits)
 
 
 
