2c7c18dd24d895654da1dea70a45bf27e92127f5
Damien LaRocque <phicoltan@gmail.com>
2019-08-09 15:13:51 -0400

    Add new examples

--- exemples.m
+++ exemples.m
@@ -54,29 +54,67 @@ ICIMAR('Planner_Critere_mid_Prismatic', 'Exemple1', axisLimits)
 
 %
 % Exemple 2 - Atteindre un point avec un robot R x n
-%   Passage dans les obstacles
+%   Passage dans et entre les obstacles
 %
 
 % Architecture : ['R'] * n
-% Longueurs : [1.5 * pi / n] * n
-% Angles : [-90] + [pi / n] * 19
-% Vitesse cartésiennes maximales de l'organe terminal : [0.05, 0.05, 3]
+% Longueurs : [2.5 * pi / n] * n
+% Angles : [180] + [360 / n] * (n - 1)
+% Vitesse cartésiennes maximales de l'organe terminal : [0.02, 0.03, 3]
 % Cibles à atteindre : [
 %     {
 %         x : 1,
-%         y : 1,
-%         angle : 270
+%         y : 0,
+%         angle : 90
 %     },
 %     {
-%         x : 1.5,
-%         y : 1.5,
+%         x :   1,
+%         y : 3.5,
+%         angle : 90
+%     },
+%     {
+%         x :   1,
+%         y : 3.5,
 %         angle : 180
+%     },
+%     {
+%         x :  -2,
+%         y : 3.5,
+%         angle : 180
+%     },
+%     {
+%         x : -0.5,
+%         y :    2,
+%         angle : 270
+%     }
+% ]
+%
+% Obstacles à éviter : [
+%     {
+%         x : 1.25,
+%         y :    0,
+%         rayon : 0.1
+%     },
+%     {
+%         x : 0.75,
+%         y :    1,
+%         rayon : 0.1
+%     },
+%     {
+%         x : 1.25,
+%         y :    2,
+%         rayon : 0.1
+%     },
+%     {
+%         x : 0.75,
+%         y :    3,
+%         rayon : 0.1
 %     }
 % ]
 
 axisLimits = [
-    [-1, 3];
-    [-2, 3]
+    [-3, 3];
+    [-2, 4]
 ];
 
 
@@ -84,16 +122,16 @@ axisLimits = [
 % a) Planner_PosOnly
 %
 
-% ICIMAR Planner_PosOnly Exemple2a
-ICIMAR('Planner_PosOnly', 'Exemple2a', axisLimits)
+% ICIMAR Planner_PosOnly Exemple2
+ICIMAR('Planner_PosOnly', 'Exemple2', axisLimits)
 
 
 %
 % b) Planner_Critere_Mem_Dist
 %
 
-% ICIMAR Planner_Critere_Mem_Dist Exemple2b
-ICIMAR('Planner_Critere_Mem_Dist', 'Exemple2b', axisLimits)
+% ICIMAR Planner_Critere_Mem_Dist Exemple2
+ICIMAR('Planner_Critere_Mem_Dist', 'Exemple2', axisLimits)
 
 
 
