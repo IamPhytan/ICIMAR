7b6dd631ec72aaa61c7ba491d127c5d2bd03c94d
Damien LaRocque <phicoltan@gmail.com>
2019-08-28 16:54:46 -0400

    Planner Yoshikawa

--- Planner_Critere_Mem_Dist_Yoshikawa.m
+++ Planner_Critere_Mem_Dist_Yoshikawa.m
@@ -2,10 +2,57 @@ function out = Planner_Critere_Mem_Dist_Yoshikawa(Architecture,Lengths,Angles,Cu
 % Planner_Critere_Mem_Dist_Yoshikawa
 %   Optimisation d'un critère
 
-    % Mesure de la manipulabilité
-    manipulabilite = - sqrt(abs(det(J * J')));
+    % Incrément infinitésimal
+    h=0.00001;
+
+    % Distances incrémentée et décrémentée
+    distIncrem = zeros(size(Lengths));
+    distDecrem = zeros(size(Lengths));
+
+    for idx = 1:length(Architecture)
+        tempValues = [Lengths, Angles];
+
+        incLength = tempValues(idx, 1) + h * (Architecture(idx) == 'P');
+        incAngle = tempValues(idx, 2) + h * (Architecture(idx) == 'R');
+        tempValues(idx, :) = [incLength, incAngle];
+
+        distIncrem(idx, :) = DistToJointCalculator(Obstacles, tempValues(:, 1), tempValues(:, 2));
+
+        tempValues = [Lengths, Angles];
+
+        decLength = tempValues(idx, 1) - h * (Architecture(idx) == 'P');
+        decAngle = tempValues(idx, 2) - h * (Architecture(idx) == 'R');
+        tempValues(idx, :) = [decLength, decAngle];
+
+        distDecrem(idx, :) = DistToJointCalculator(Obstacles, tempValues(:, 1), tempValues(:, 2));
+    end
+
+
+    discriminator = double(distIncrem ~= 0 & distDecrem ~= 0);
+    distIncrem = distIncrem .* discriminator;
+    distDecrem = distDecrem .* discriminator;
+
+    distDot=(distIncrem-distDecrem)/(2*h);
+
+    % if (norm(DistDot) > 1E-6)
+    %     DistDot=DistDot/norm(DistDot);
+    % else
+    %     DistDot=zeros(size(Architecture));
+    % end
+
+    % Pseudo Inverse
+    Jinv = J' / (J * J');
+
+    % Projection dans le Noyau
+    Jnoy = (eye(size(Jinv * J)) - Jinv*J);
 
     % Position envoyée au robot
-    out = CurrentJointValues + manipulabilite * xyThetaDot;
+
+    K = 0.35; % Gain de la projection
+
+    out = CurrentJointValues + Jinv * xyThetaDot + K * Jnoy * distDot;
+
+    %out=CurrentJointAngles;
+
 end
 
