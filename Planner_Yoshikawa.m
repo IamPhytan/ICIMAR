b9da869fb4f1b11dbb748194c49dab7633ed1c68
Damien LaRocque <phicoltan@gmail.com>
2019-08-28 11:59:38 -0400

    EX03 - Ajout d'un planner Yoshikawa

---
+++ Planner_Yoshikawa.m
@@ -0,0 +1,11 @@
+function out = Planner_Yoshikawa(Architecture,Lengths,Angles,CurrentJointValues,J,Obstacles,xyThetaDot)
+% Planner_Yoshikawa
+%   Optimisation d'un critère
+
+    % Mesure de la manipulabilité
+    manipulabilite = - sqrt(det(J * J'));
+
+    % Position envoyée au robot
+    out = CurrentJointValues + manipulabilite * xyThetaDot;
+end
+
